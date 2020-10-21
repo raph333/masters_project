fixed distance normalization bug

graph_conv_net/experiments/distance_threshold.py
@@ -13,26 +13,28 @@ AlchemyCompetitionDataset.data_processor = TencentDataProcessor()
 
 def get_transform(threshold: float) -> Callable:
     return AddEdges(distance_threshold=threshold,
-                    add_dist_feature=True)
+                    add_dist_feature=True,
+                    norm_dist=False,
+                    allow_removal_of_original_edges=False)
 
 
 new_config = {
-    'name': 'NE-real-root-weight',  # todo: set this for each experiment!  (default: 'test-run')
+    'name':  'test-run',  #'NE-real-fix',  # todo: set this for each experiment!  (default: 'test-run')
     'target_param': {
         'name': 'distance_threshold',
-        'values': [None, 1.5, 2, 3, 4, 5, np.inf]
+        'values': [None, 2, 3, 4, 5, np.inf]
     },
     'lr_scheduler': {
         'class': torch.optim.lr_scheduler.ExponentialLR,
-        'kwargs': {'gamma': 0.99}
+        'kwargs': {'gamma': 0.995}
     },
     'dataset_class': AlchemyCompetitionDataset,
     'get_transform': get_transform,
-    'repeat': 3,
+    'repeat': 1, # 3,  todo: update
     'lr':  0.001,
     'model_name': 'tencent_mpnn',
-    'num_epochs': 200,
-    'cuda': 2
+    'num_epochs': 3,  #150,  todo: update
+    'cuda': 0
 }
 CONFIG.update(new_config)
 

graph_conv_net/experiments/single_run.py
@@ -4,40 +4,46 @@ import torch
 
 from graph_conv_net.train import run_experiment
 from graph_conv_net.alchemy_dataset import AlchemyCompetitionDataset
-from graph_conv_net.data_processing import SchNetDataProcessor
+from graph_conv_net.data_processing import TencentDataProcessor
+# from graph_conv_net.data_processing import SchNetDataProcessor
 from graph_conv_net.transformations import AddEdges
 from experiments.distance_threshold import CONFIG
 
-AlchemyCompetitionDataset.data_processor = SchNetDataProcessor()
+AlchemyCompetitionDataset.data_processor = TencentDataProcessor()
+# AlchemyCompetitionDataset.data_processor = SchNetDataProcessor()
 
 
 def get_transform(threshold: float) -> Callable:
     return AddEdges(distance_threshold=threshold,
-                    add_dist_feature=True)
+                    add_dist_feature=True,
+                    norm_dist=False)
 
 
 new_config = {
     'name': 'test-run',  # todo: set this for each experiment!  (default 'test-run')
     'dataset_class': AlchemyCompetitionDataset,
-    'data_processor': SchNetDataProcessor,
+    'data_processor': TencentDataProcessor,  # SchNetDataProcessor,
     'get_transform': get_transform,
     'target_param': {  # usually a parameter for the transformation
         'name': 'distance_threshold',
-        'values': [np.inf]
+        'values': [None]
     },
-    'batch_size': 1,  # todo: remove (replace by base config) after testing
     # 'lr_scheduler': {
     #     'class': torch.optim.lr_scheduler.ReduceLROnPlateau,
     #     'kwargs': {'factor': 0.75,
     #                'threshold': 1e-4,
     #                'patience': 6}
     # },
+    'batch_size': 1,
+    'lr': 0.01,  # start high
     'lr_scheduler': {
         'class': torch.optim.lr_scheduler.ExponentialLR,
-        'kwargs': {'gamma': 0.99}
+        'kwargs': {'gamma': 0.97  # ... and decrease quickly
+                   }
+        # after the test run: figure out a better starting learning rate and decrease more slowly
     },
     'cuda': 3,
-    'repeat': 3,
+    'repeat': 1,
     'num_epochs': 200
 }
 CONFIG.update(new_config)

graph_conv_net/graph_conv_net/transformations.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional
 from itertools import product
 import numpy as np
 
@@ -70,34 +70,52 @@ class AddEdges:
     """
 
     def __init__(self,
-                 distance_threshold: Union[float, None] = np.inf,
+                 distance_threshold: Optional[float] = np.inf,
                  norm_dist: bool = True,
-                 add_dist_feature: bool = False):
-
-        if distance_threshold is None:
-            self.add_edges = False
-        else:
-            self.add_edges = True
+                 add_dist_feature: bool = False,
+                 allow_removal_of_original_edges: bool = False):
 
         self.t = distance_threshold
+        self.norm_dist = norm_dist
         self.add_dist_feature = add_dist_feature
+        self.remove_edges_ok = allow_removal_of_original_edges
 
         self.calculate_complete_graph = CompleteGraph()
-        self.calculate_distance = Distance(norm=norm_dist)
+        self.calculate_distance = Distance(norm=False)
         self.apply_distance_threshold = DistanceThreshold(threshold=self.t)
 
     def __call__(self, data: Data) -> Data:
-        if self.add_edges is False:
+
+        # no edges to add:
+        if self.t is None or self.t == 0:
+
+            if self.add_dist_feature:
+                data = self.calculate_distance(data)
+
+                if self.norm_dist:
+                    max_dist = data.edge_attr[:, -1].max()
+                    data.edge_attr[:, -1] = data.edge_attr[:, -1] / max_dist
+                    # problem: manual normalization is very slow
+                    # maybe calculate normalized distance threshold first and use build-in normalization
+
             return data
 
+        # add edges:
+        num_original_edges = data.edge_attr.shape[0]
         data = self.calculate_complete_graph(data)
         data = self.calculate_distance(data)
         data = self.apply_distance_threshold(data)
 
-        if self.add_dist_feature:
-            return data
+        if data.edge_attr.shape[0] < num_original_edges and self.remove_edges_ok is False:
+            raise AssertionError(f'Original edges are removed with threshold {self.t}')
 
-        data.edge_attr = data.edge_attr[:, :-1]  # remove distance column
+        if self.add_dist_feature is False:
+            data.edge_attr = data.edge_attr[:, :-1]  # remove distance column
+
+        elif self.norm_dist:
+            max_dist = data.edge_attr[:, -1].max()
+            data.edge_attr[:, -1] = data.edge_attr[:, -1] / max_dist
+
         return data
 
     def __repr__(self):